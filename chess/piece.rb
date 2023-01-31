84acdf28d20cd38b4e46567c28089a6e463e5633
“Lissa <lwarshaw674@gmail.com>
2023-01-31 15:40:55 -0500

    second push

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,3 +1,56 @@
+require_relative 'chess_modules'
+
 class Piece
-  
+
+  def initialize(color, board, pos)
+    
+    @color = symbol
+    @board = Board 
+    @pos = pos 
+
+  end
+
+  def moves
+
+  end
+
+
+
 end
+
+
+class Rook < Piece
+  include Slideable
+end
+
+
+class Queen < Piece
+  include Slideable
+end
+
+
+class Bishop < Piece
+  include Slideable
+end
+
+
+class King < Piece
+  include Stepable
+end
+
+
+class Knight < Piece
+  include Stepable
+end
+
+
+class Pawn < Piece
+
+end
+
+
+class NullPiece < Piece
+  include Singleton
+end
+
+
