beb22dde31fdc97bcc1ef564a8210573b6672fae
“Lissa <lwarshaw674@gmail.com>
2023-02-01 15:23:51 -0500

    push 3

--- chess/piece.rb
+++ chess/piece.rb
@@ -158,19 +158,46 @@ class Pawn < Piece
 
   private
   def at_start_row?
-
+    return true if pos[0] == 1 || pos[0] == 6 
+    false 
   end
 
   def forward_dir
+    color == :white ? -1 : 1
 
   end
 
   def forward_steps
+    considered_moves = []
+    dx = forward_dir
+    
+    considered_pos = pos.dup
+    considered_pos[0] += dx
 
+    if !blocked(considered_pos) 
+      considered_moves << considered_pos.dup
+      
+      considered_pos[0] += dx
+      if !blocked(considered_pos) && at_start_row?
+        considered_moves << considered_pos.dup 
+      end
+    end
+   
+    return considered_moves 
   end
 
   def side_attacks
 
+  end
+
+  def blocked?(pos)
+    return true if pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7 || (board[pos].color == color) 
+    false
+  end
+
+  def enemy?(pos)
+    return true if board[pos].color != color
+    false
   end
 end
 
